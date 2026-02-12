9fd8368ef8f1fb9c0c6edf0d09fa7cbcc1370224
Yvonne Ntinyari <ntinyariyvonne823@gmail.com>
2026-02-12 22:05:31 +0300

    added certifications

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -16,6 +16,8 @@ const Resume = () => (
               <li><span className="font-bold">BSc in Information Security and Forensics</span> - KCA University (2021-2025)</li>
               <li><span className="font-bold">Web Development Bootcamp</span> - Online (2022)</li>
               <li><span className="font-bold">Cisco Certification</span> - Cisco (Year)</li>
+              <li><span className="font-bold">CyberSecurity</span> -ALX (2025-2026)</li> 
+              <li><span className="font-bold">Front-End Web Development</span> -ALX(2025-2026)</li>
             </ul>
           </div>
         </FadeInSection>
