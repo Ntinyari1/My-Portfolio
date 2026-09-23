d66a4a02fed470ae0c2d033f319fd58736f0602c
Yvonne Ntinyari <ntinyariyvonne823@gmail.com>
2026-09-23 14:01:06 +0300

    Fix spacing in education list items
    Corrected spacing in education list items for consistency.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,8 +14,8 @@ const Resume = () => (
             <h3 className="text-xl md:text-2xl font-semibold mb-4 text-accent-orange">Education</h3>
             <ul className="text-gray-300 space-y-2">
               <li><span className="font-bold">BSc in Information Security and Forensics</span> - KCA University (2022-2026)</li>
-              <li><span className="font-bold">CyberSecurity</span> -ALX (2025-2026)</li> 
-              <li><span className="font-bold">Front-End Web Development</span> -ALX(2025-2026)</li>
+              <li><span className="font-bold">CyberSecurity</span> - ALX (2025-2026)</li> 
+              <li><span className="font-bold">Front-End Web Development</span> - ALX(2025-2026)</li>
              <li><span className="font-bold">ALX Professional Foundations</span> - (2025)</li>
             <li> <span className="font-bold">Cisco Certified Network Associate (CCNA)</span> – Cisco (2023)</li>
              <li><span className="font-bold">Web Development Bootcamp</span> - Online (2022)</li>
